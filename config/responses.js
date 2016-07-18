4b6ea1a5a8395e7564c401734b549786c596b798
swhudson <scott.w.hudson@gmail.com>
2016-07-18 13:31:09 -0500

    add query param sorting, select fields, and limit

--- config/responses.js
+++ config/responses.js
@@ -2,20 +2,71 @@
 
 const Boom = require('boom');
 
+// Default number of records returned if no limit is specified
+const API_LIMIT = 100;
+
 module.exports = {
 
   // fetch all records for given class, with optional
   // filtering params
   //
   indexAction: function (klass, request, reply) {
+    queryDatastore(klass, request, reply, API_LIMIT);
+  },
 
-    const query = request.url.query;
+  // fetch single record matching specified ID
+  //
+  showAction: function (klass, request, reply) {
+    queryDatastore(klass, request, reply, 1);
+  }
+};
 
-    klass.find(query, function (error, data) {
-      if (error || data.length === 0) {
-        reply(
-          Boom.notFound('missing');
-        );
+const queryDatastore = function (klass, request, reply, limit) {
+
+  var params = request.url.query;
+  var fields = '';
+  var order_by = '';
+  var queryLimit = limit;
+
+  if (Object.keys(params).length > 0) {
+    if (Object.prototype.hasOwnProperty.call(params, 'fields')) {
+      fields = params.fields.replace(',', ' ');
+      delete params.fields;
+    }
+
+    if (Object.prototype.hasOwnProperty.call(params, 'order_by')) {
+      order_by = params.order_by;
+      delete params.order_by;
+    }
+
+    if (Object.prototype.hasOwnProperty.call(params, 'limit')) {
+      queryLimit = parseInt(params.limit);
+      delete params.limit;
+    }
+  }
+
+  klass.
+    find(params).
+    limit(queryLimit).
+    sort(order_by).
+    select(fields).
+    exec(function (error, data) {
+      console.log('QUERY LOG');
+      console.log('class');
+      console.log(klass);
+      console.log('params');
+      console.log(params);
+      console.log('fields');
+      console.log(fields);
+      console.log('limit');
+      console.log(queryLimit);
+      console.log('records retrieved');
+      console.log(data.length);
+      console.log('error');
+      console.log(error);
+
+      if (data.length === 0) {
+        reply(Boom.notFound('missing'));
       } else {
         reply({
           meta: {
@@ -27,18 +78,5 @@ module.exports = {
         });
       }
     });
-  },
-
-  // fetch single record matching specified ID
-  //
-  showAction: function (klass, request, reply) {
-    klass.find(request.params, function (error, data) {
-      if (error) {
-        reply(Boom.notFound('missing'));
-      } else {
-        reply(data[0]);
-      }
-    });
-  }
 
 };
